b166e427f4f7eb51299a74d241096f64da73a73a
Your Name <you@example.com>
2023-05-06 01:12:22 +0800

    1111

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,6 +1,6 @@
 sudo docker rm -f $(sudo docker ps -aq)
-sudo docker network prune
-sudo docker volume prune
+sudo docker network prune -f
+sudo docker volume prune -f
 cd fixtures && docker-compose up -d
 cd ..
 rm education
